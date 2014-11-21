bfc485b996675ab9450359898c98a8f7b56241d6
Eddy0402 <yukkcake@gmail.com>
2014-11-22 00:00:15 +0800

    Implement `send_btye` and `recv_btye` for shell
    Not properly dealing with concurrency, but currently no multiple
    receiver will wait for data in the same time.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
+#include "semphr.h"
 
 #include "main.h"
 #include "game_task.h"
@@ -25,12 +27,16 @@ void RCC_Configuration(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
     /* GPIOA clock enable */
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+    /* GPIOA clock enable */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
 }
 
 void GPIO_Configuration(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
+    /*-------------------------- GPIO Configuration ----------------------------*/
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -39,23 +45,14 @@ void GPIO_Configuration(void)
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
     /* Connect USART pins to AF */
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);//USART1_TX
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);//USART1_RX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);   // USART1_TX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);  // USART1_RX
 }
 
 void USART1_Configuration(void)
 {
     USART_InitTypeDef USART_InitStructure;
 
-    /* USARTx configuration 
-     * USARTx configured as follow:
-     *- BaudRate = 9600 baud
-     *- Word Length = 8 Bits
-     *- One Stop Bit
-     *- No parity
-     *- Hardware flow control disabled (RTS and CTS signals)
-     *- Receive and transmit enabled
-     **/
     USART_InitStructure.USART_BaudRate = 115200;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits = USART_StopBits_1;
@@ -65,6 +62,44 @@ void USART1_Configuration(void)
     USART_Init(USART1, &USART_InitStructure);
     USART_Cmd(USART1, ENABLE);
 }
+
+void USART_IT_CONFIG(){
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+volatile xSemaphoreHandle serial_tx_wait_sem = NULL;
+volatile xQueueHandle serial_rx_queue = NULL;
+
+void USART1_IRQHandler()
+{
+    static signed portBASE_TYPE xHigherPriorityWoken;
+    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET){
+        char msg = USART_ReceiveData(USART1);
+        if(!xQueueSendToBack(serial_rx_queue, &msg, portMAX_DELAY));
+    }
+}
+
+void send_byte(char ch){
+    while(!xSemaphoreTake(serial_tx_wait_sem, portMAX_DELAY));
+    while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+    USART_SendData(USART1, ch);
+    xSemaphoreGive(serial_tx_wait_sem);
+}
+
+char recv_byte(){
+    char msg;
+    USART_IT_CONFIG(USART1, USART_IT_RXNE, ENABLE);
+    while(!xQueueReceive(serial_rx_queue,&msg,portMAX_DELAY));
+    return msg;
+}
+
 
 void prvInit(){
     //LCD init
@@ -80,17 +115,13 @@ void prvInit(){
     //Button
     STM_EVAL_PBInit( BUTTON_USER, BUTTON_MODE_GPIO );
 
+    vSemaphoreCreateBinary(serial_tx_wait_sem);
+    serial_rx_queue = xQueueCreate(1,sizeof(char));
+
     RCC_Configuration();
     GPIO_Configuration();
     USART1_Configuration();
-}
-
-void send_byte(char ch){
-    // TODO: implementation
-}
-
-char recv_byte(){
-    // TODO: implementation
+    USART_IT_CONFIG();
 }
 
 void command_prompt(void *pvParameters){
@@ -117,6 +148,13 @@ void command_prompt(void *pvParameters){
 //Main Function
 int main(void)
 {
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode=GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_Pin=GPIO_Pin_13;
+    GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
+
+    GPIO_Init(GPIOG, &GPIO_InitStructure);
     prvInit();
     fs_init();
     fio_init();
@@ -125,12 +163,11 @@ int main(void)
     RNG_Cmd(ENABLE);
 
     xTaskCreate(command_prompt, (char *) "cli", 512,
-            NULL, tskIDLE_PRIORITY + 1, NULL);
+            NULL, tskIDLE_PRIORITY + 2, NULL);
     register_romfs("romfs", &_sromfs);
     register_devfs();
 
     create_game_tasks();
-
     //Call Scheduler
     vTaskStartScheduler();
 }
